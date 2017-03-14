8f9e33568309daf7f903ce598a2e805818da9c9f
legokichi <legokichi@gmail.com>
2017-03-14 19:58:20 +0900

    cnn demo need more memory

--- build.sh
+++ build.sh
@@ -39,15 +39,23 @@ emcc \
   -std=c++1z \
   -DDLIB_JPEG_SUPPORT \
   -O2  --llvm-lto 1 \
+  --bind \
   -o ./iface.bc
-# MUST need "-DDLIB_JPEG_SUPPORT" option - https://sourceforge.net/p/dclib/discussion/442518/thread/93deb3e9/#178a/3589/57e7/8d47
+
+# MUST need "-DDLIB_JPEG_SUPPORT" option
+# https://sourceforge.net/p/dclib/discussion/442518/thread/93deb3e9/#178a/3589/57e7/8d47
+# http://dlib.net/faq.html#Howdoesdlibinterfacewithotherlibrariestools
 
 echo preload files
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
 wget http://dlib.net/files/mmod_human_face_detector.dat.bz2
 bzip2 -d mmod_human_face_detector.dat.bz2
-cp ../examples/faces/2008_002470.jpg ./2008_002470.jpg
+# http://qiita.com/SousukeShimoyama/items/a6b853a1575623f603cf
+#wget  http://dlib.net/files/data/dlib_face_detection_dataset-2016-09-30.tar.gz
+#tar xzvf dlib_face_detection_dataset-2016-09-30.tar.gz
+#cp ./dlib_face_detection_dataset/faces_2016_09_30.xml ./
+cp ../examples/faces/2008_002470.jpg ./
 
 echo emitting to js
 # http://dlib.net/files/
@@ -60,8 +68,17 @@ emcc \
   --preload-file ./2008_002470.jpg \
   --preload-file ./shape_predictor_68_face_landmarks.dat \
   --preload-file ./mmod_human_face_detector.dat \
-  -O2 --llvm-lto 3 \
+  -O2 --llvm-lto 1 \
+  --bind \
   -s ASSERTIONS=0 \
-  -s ALLOW_MEMORY_GROWTH=1 \
+  -s TOTAL_MEMORY=2147483648 \
+  -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=4 \
   -s DISABLE_EXCEPTION_CATCHING=0 \
   -o ../../dlib.html
+# ALLOW_MEMORY_GROWTH と USE_PTHREADS は両立できない - https://groups.google.com/forum/#!topic/emscripten-discuss/m10q3iDixnQ
+# -s SPLIT_MEMORY=1073741824 \
+# メモリが足りない人々の嘆き - https://bugs.chromium.org/p/chromium/issues/detail?id=331660
+# SPLIT_MEMORY と USE_PTHREADS は両立できない - https://github.com/kripken/emscripten/wiki/Split-Memory
+# -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=4 \
+# sed -in-place 's/PthreadWorkerInit:PthreadWorkerInit});PThread.unusedWorkerPool.push/PthreadWorkerInit:PthreadWorkerInit},[HEAPU8.buffer]);PThread.unusedWorkerPool.push/' ../../dlib.js
+# -s 'EXPORTED_FUNCTIONS=["_cnn_face_detector", "_hog_face_detector"]' \
--- iface.cpp
+++ iface.cpp
@@ -1,14 +1,16 @@
 #include <dlib/image_processing/object_detector.h>
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing.h>
+#include <dlib/dnn.h>
 #include <dlib/image_io.h>
 #include <iostream>
+#include <emscripten/bind.h>
 
 using std::cout;
 using std::endl;
 using namespace dlib;
 
-int main(){ try{
+int hog_face_detector(){ try{
   // http://dlib.net/face_landmark_detection_ex.cpp.html
   frontal_face_detector detector = get_frontal_face_detector();
   cout << "detector" << endl;
@@ -37,4 +39,51 @@ int main(){ try{
 }catch(std::exception& e){
   cout << "\nexception thrown!" << endl;
   cout << e.what() << endl;
+  return 1;
 } }
+
+// http://dlib.net/dnn_mmod_face_detection_ex.cpp.html
+// http://dlib.net/files/data/dlib_face_detection_dataset-2016-09-30.tar.gz
+// faces_2016_09_30.xml
+template <long num_filters, typename SUBNET> using con5d = con<num_filters,5,5,2,2,SUBNET>;
+template <long num_filters, typename SUBNET> using con5  = con<num_filters,5,5,1,1,SUBNET>;
+template <typename SUBNET> using downsampler  = relu<affine<con5d<32, relu<affine<con5d<32, relu<affine<con5d<16,SUBNET>>>>>>>>>;
+template <typename SUBNET> using rcon5  = relu<affine<con5<45,SUBNET>>>;
+using net_type = loss_mmod<con<1,9,9,1,1,rcon5<rcon5<rcon5<downsampler<input_rgb_image_pyramid<pyramid_down<6>>>>>>>>;
+
+int cnn_face_detector(){ try{
+  net_type net;
+  deserialize("./mmod_human_face_detector.dat") >> net;
+  cout << "deserialize" << endl;
+
+  matrix<rgb_pixel> img;
+  load_image(img, "./2008_002470.jpg");
+  cout << "img" << endl;
+
+  while(img.size() < 1800*1800){ pyramid_up(img); }
+  cout << "pyramid_up" << endl;
+
+  const std::vector<mmod_rect> dets = net(img);
+  cout << "net" << endl;
+
+  for (auto&& d : dets){
+    auto rect = d.rect;
+    cout
+      << "(" << rect.left() << "," << rect.top() << ")"
+      << ","
+      << "(" << rect.right() << "," << rect.bottom() << ")"
+      << endl;
+  }
+
+  return 0;
+}catch(std::exception& e){
+  cout << "\nexception thrown!" << endl;
+  cout << e.what() << endl;
+  return 1;
+} }
+
+
+EMSCRIPTEN_BINDINGS(dlib) {
+  emscripten::function("hog_face_detector", &hog_face_detector);
+  emscripten::function("cnn_face_detector", &cnn_face_detector);
+}
